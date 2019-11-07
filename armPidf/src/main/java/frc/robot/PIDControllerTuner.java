bff72a95af50cb8d3d3eb086791236e8087468f5
Peter Donovan <33707478+petervdonovan@users.noreply.github.com>
2019-11-06 21:42:10 -0800

    Update PIDControllerTuner.java

--- armPidf/src/main/java/frc/robot/PIDControllerTuner.java
+++ armPidf/src/main/java/frc/robot/PIDControllerTuner.java
@@ -12,7 +12,7 @@ public class PIDControllerTuner {
     public PIDControllerTuner(PIDController pid, XboxController joy) {
         this.pid = pid;
         this.joy = joy;
-        this.maxKChangePerSecond = 0.5;
+        this.maxKChangePerSecond = 0.15;
     }
     public PIDControllerTuner(PIDController pid, XboxController joy, double maxKChangePerSecond) {
         this.pid = pid;
@@ -20,7 +20,7 @@ public class PIDControllerTuner {
         this.maxKChangePerSecond = maxKChangePerSecond;
     }
     public void update() {
-        double changeRequest = Math.pow(joy.getY(Hand.kLeft), 3) * maxKChangePerSecond;
+        double changeRequest = curve(joy.getY(Hand.kLeft)) * maxKChangePerSecond / 50;
         if(joy.getAButton()) { //User wants to set Kf
             pid.setF(pid.getF() + changeRequest);
         }
@@ -41,8 +41,11 @@ public class PIDControllerTuner {
         SmartDashboard.putNumber("Kp", pid.getP());
         SmartDashboard.putNumber("Kd", pid.getD());
         SmartDashboard.putNumber("Ki", pid.getI());
-
+        SmartDashboard.putNumber("Setpoint", pid.getSetpoint());
         //Put error to SmartDashboard. Recommended that they are graphs with respect to time
         SmartDashboard.putNumber("Error", pid.getError());
+    }
+    private double curve(double input) {
+        return Math.pow(input, 2) * input / Math.abs(input);
     }
 }
